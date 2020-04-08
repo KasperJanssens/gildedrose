b19b4a101059551e0a43c208ec79e78e81f8e7ca
Kasper Janssens <kasper@krondorsoft.com>
2020-04-08 17:40:09 +0200

    Simplify backstage pass

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -64,26 +64,19 @@ class GildedRose {
 
         if (isBackstagePass(item)) {
             item.sellIn = item.sellIn - 1;
-
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.sellIn < 10) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                if (item.sellIn < 5) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+            if(item.sellIn < 0){
+                item.quality = MIN_QUALITY;
+            }
+            if(item.sellIn >= 0 && item.sellIn < 5){
+                item.quality = Math.min(MAX_QUALITY, item.quality  +3);
+            }
+            if(item.sellIn >=5 && item.sellIn <10) {
+                item.quality = Math.min(MAX_QUALITY, item.quality  +2);
+            }
+            if(item.sellIn >=10){
+                item.quality = Math.min(MAX_QUALITY, item.quality  +1);
             }
 
-            if (item.sellIn < 0) {
-                item.quality = 0;
-            }
         }
 
     }
