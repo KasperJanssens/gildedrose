f68714ffca059ba9b73365cb42b1b30df04f8944
Kasper Janssens <kasper@krondorsoft.com>
2020-04-08 13:39:23 +0200

    Add tests for backstage passes too

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,6 +10,7 @@ class GildedRoseTest {
     public static final int SULFURAS_QUALITY = 80;
     public static final String AGED_BRIE = "Aged Brie";
     public static final int MAX_QUALITY= 50;
+    private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
 
     @Test
     void testQualityDegradation_normalElement () {
@@ -114,6 +115,117 @@ class GildedRoseTest {
         GildedRose.updateSingleItem(item);
         assertThat(item.quality).isEqualTo(MAX_QUALITY);
         assertThat(item.sellIn).isEqualTo(2);
+    }
+
+    @Test
+    void backstagePasses_moreThan10DaysLeftNormalIncrease () {
+        Item item = new Item(BACKSTAGE_PASS, 13, 20);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(21);
+        assertThat(item.sellIn).isEqualTo(12);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(22);
+        assertThat(item.sellIn).isEqualTo(11);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(23);
+        assertThat(item.sellIn).isEqualTo(10);
+    }
+
+    @Test
+    void backstagePasses_lessThan10DaysButMoreThan5Left () {
+        Item item = new Item(BACKSTAGE_PASS, 10, 20);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(22);
+        assertThat(item.sellIn).isEqualTo(9);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(24);
+        assertThat(item.sellIn).isEqualTo(8);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(26);
+        assertThat(item.sellIn).isEqualTo(7);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(28);
+        assertThat(item.sellIn).isEqualTo(6);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(30);
+        assertThat(item.sellIn).isEqualTo(5);
+    }
+
+    @Test
+    void backstagePasses_lessThan5DaysButMoreThan0Left () {
+        Item item = new Item(BACKSTAGE_PASS, 5, 20);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(23);
+        assertThat(item.sellIn).isEqualTo(4);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(26);
+        assertThat(item.sellIn).isEqualTo(3);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(29);
+        assertThat(item.sellIn).isEqualTo(2);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(32);
+        assertThat(item.sellIn).isEqualTo(1);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(35);
+        assertThat(item.sellIn).isEqualTo(0);
+    }
+
+
+    @Test
+    void backstagePasses_noMoreThanMaxQuality_lowestSellin () {
+        Item item = new Item(BACKSTAGE_PASS, 5, MAX_QUALITY);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+        assertThat(item.sellIn).isEqualTo(4);
+
+    }
+    @Test
+    void backstagePasses_noMoreThanMaxQuality_mediumSellin () {
+        Item item = new Item(BACKSTAGE_PASS, 10, MAX_QUALITY);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+        assertThat(item.sellIn).isEqualTo(9);
+
+    }
+
+    @Test
+    void backstagePasses_noMoreThanMaxQuality_highestSellin () {
+        Item item = new Item(BACKSTAGE_PASS, 15, MAX_QUALITY);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+        assertThat(item.sellIn).isEqualTo(14);
+
+    }
+
+    @Test
+    void backstagePasses_noValueAfterSellinDate () {
+        Item item = new Item(BACKSTAGE_PASS, 1, 42);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(45);
+        assertThat(item.sellIn).isEqualTo(0);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(-1);
+
     }
 
 }
