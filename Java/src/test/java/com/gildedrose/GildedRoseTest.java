55073579e0d9acb74e0ab008162390cac5e7b034
Kasper Janssens <kasper@krondorsoft.com>
2020-04-08 13:07:57 +0200

    Add tests for Sulfuras and Aged Brie

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,15 +6,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class GildedRoseTest {
 
-    String sulfuras = "Sulfuras";
-
-//    @Test
-//    void foo() {
-//        Item[] items = new Item[] { new Item("foo", 0, 0) };
-//        GildedRose app = new GildedRose(items);
-//        app.updateQuality();
-//        assertEquals("fixme", app.items[0].name);
-//    }
+    public static final String HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final int SULFURAS_QUALITY = 80;
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final int MAX_QUALITY= 50;
 
     @Test
     void testQualityDegradation_normalElement () {
@@ -56,6 +51,69 @@ class GildedRoseTest {
         GildedRose.updateSingleItem(item);
         assertThat(item.quality).isEqualTo(0);
         assertThat(item.sellIn).isEqualTo(-4);
+    }
+
+    @Test
+    void testSulfuras_NeverDegradesInQualityOrSellinDate () {
+        Item item = new Item(HAND_OF_RAGNAROS, 2, SULFURAS_QUALITY);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(SULFURAS_QUALITY);
+        assertThat(item.sellIn).isEqualTo(2);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(SULFURAS_QUALITY);
+        assertThat(item.sellIn).isEqualTo(2);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(SULFURAS_QUALITY);
+        assertThat(item.sellIn).isEqualTo(2);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(SULFURAS_QUALITY);
+        assertThat(item.sellIn).isEqualTo(2);
+    }
+
+    @Test
+    void testAgedBrie_IncreasesInQualityAsItgetsOlder () {
+        Item item = new Item(AGED_BRIE, 6, 2);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(3);
+        assertThat(item.sellIn).isEqualTo(5);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(4);
+        assertThat(item.sellIn).isEqualTo(4);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(5);
+        assertThat(item.sellIn).isEqualTo(3);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(6);
+        assertThat(item.sellIn).isEqualTo(2);
+    }
+
+    @Test
+    void testAgedBrie_NeverGoesBeyondMaxQuality () {
+        Item item = new Item(AGED_BRIE, 6, MAX_QUALITY - 1);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+        assertThat(item.sellIn).isEqualTo(5);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+        assertThat(item.sellIn).isEqualTo(4);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+        assertThat(item.sellIn).isEqualTo(3);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+        assertThat(item.sellIn).isEqualTo(2);
     }
 
 }
