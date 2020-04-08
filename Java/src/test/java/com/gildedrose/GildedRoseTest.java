99f02683276be824eb0c57a5737591623735a1b1
Kasper Janssens <kasper@krondorsoft.com>
2020-04-08 12:49:08 +0200

    Write simple tests for normal items

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,60 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GildedRoseTest {
 
+    String sulfuras = "Sulfuras";
+
+//    @Test
+//    void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("fixme", app.items[0].name);
+//    }
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void testQualityDegradation_normalElement () {
+        Item item = new Item("foo", 2, 2);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(1);
+        assertThat(item.sellIn).isEqualTo(1);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(0);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(-1);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(-2);
+    }
+
+    @Test
+    void testQualityDegradation_normalElementQualityDegradesDoubleAfterSellingDate () {
+        Item item = new Item("foo", 0, 4);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(2);
+        assertThat(item.sellIn).isEqualTo(-1);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(-2);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(-3);
+
+        GildedRose.updateSingleItem(item);
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(-4);
     }
 
 }
